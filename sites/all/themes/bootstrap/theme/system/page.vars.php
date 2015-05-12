d1989962eea0cc3f279be320de2fd63a46072dcf
Nosalyuk Sergiy <nosalyuk.sergiy@iron-eggs.com>
2015-05-12 20:49:01 +0300

    contact popup

--- sites/all/themes/bootstrap/theme/system/page.vars.php
+++ sites/all/themes/bootstrap/theme/system/page.vars.php
@@ -12,13 +12,13 @@
 function bootstrap_preprocess_page(&$variables) {
   // Add information about the number of sidebars.
   if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
-    $variables['content_column_class'] = ' class="col-sm-6"';
+    $variables['content_column_class'] = ' class="col-xs-6"';
   }
   elseif (!empty($variables['page']['sidebar_first']) || !empty($variables['page']['sidebar_second'])) {
-    $variables['content_column_class'] = ' class="col-sm-9"';
+    $variables['content_column_class'] = ' class="col-xs-9"';
   }
   else {
-    $variables['content_column_class'] = ' class="col-sm-12"';
+    $variables['content_column_class'] = ' class="col-xs-12"';
   }
 
   // Primary nav.
